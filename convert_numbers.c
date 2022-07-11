2933f9638dbeca66b006f584f376a33ab3f02b64
Siraaj <sirjeftha@gmail.com>
2022-07-11 14:23:02 -0700

    added conversion specifiers

--- convert_numbers.c
+++ convert_numbers.c
@@ -1,7 +1,14 @@
 #include "main.h"
 
 unsigned int convert_di(va_list args, buffer_t *output,
-			unsigned char flags, int wid, int prec, unsigned char len);
+unsigned char flags, int wid, int prec, unsigned char len);
+unsigned int convert_b(va_list args, buffer_t *output,
+unsigned char flags, int wid, int prec, unsigned char len);
+unsigned int convert_u(va_list args, buffer_t *output,
+unsigned char flags, int wid, int prec, unsigned char len);
+unsigned int convert_o(va_list args, buffer_t *output,
+unsigned char flags, int wid, int prec, unsigned char len);
+
 /**
  * convert_di - Converts an argument to a signed int and
  *              stores it to a buffer contained in a struct.
@@ -33,7 +40,7 @@ d = (short)d;
 if (SPACE_FLAG == 1 && d >= 0)
 ret += _memcpy(output, &space, 1);
 
-if (prec <= 0 && NEG_FLAG == 0) /* Handle width  */
+if (prec <= 0 && NEG_FLAG == 0) /* Condition Handles width  */
 {
 if (d == LONG_MIN)
 count += 19;
@@ -49,7 +56,7 @@ count += (SPACE_FLAG == 1 && d >= 0) ? 1 : 0;
 
 /* Handle plus flag when zero flag is active */
 if (ZERO_FLAG == 1 && PLUS_FLAG == 1 && d >= 0)
-et += _memcpy(output, &plus, 1);
+ret += _memcpy(output, &plus, 1);
 /*Print negative sign when zero flag is active */
 if (ZERO_FLAG == 1 && d < 0)
 ret += _memcpy(output, &neg, 1);
@@ -71,3 +78,95 @@ ret += convert_sbase(output, d, "0123456789", flags, 0, prec);
 ret += print_neg_width(output, ret, flags, wid);
 
 return (ret);
+
+/**
+ * convert_b - Converts an unsigned int argument into binary
+ *             and stores it to a buffer contained in a struct.
+ * @args: A va_list pointing to the argument to be converted.
+ * @flags: Flag modifiers.
+ * @wid: A width modifier.
+ * @prec: A precision modifier.
+ * @len: A length modifier.
+ * @output: A buffer_t struct containing a character array.
+ * Return: The number of bytes stored to the buffer.
+ */
+
+unsigned int convert_b(va_list args, buffer_t *output,
+unsigned char flags, int wid, int prec, unsigned char len)
+{
+unsigned int num;
+
+num = va_arg(args, unsigned int);
+
+(void)len;
+
+return (convert_ubase(output, num, "01", flags, wid, prec));
+}
+
+/**
+ * convert_o - Converts an unsigned int to octal and
+ *             stores it to a buffer contained in a struct.
+ * @args: A va_list poinitng to the argument to be converted.
+ * @flags: Flag modifiers.
+ * @wid: A width modifier.
+ * @prec: A precision modifier.
+ * @len: A length modifier.
+ * @output: A buffer_t struct containing a character array.
+ * Return: The number of bytes stored to the buffer.
+ */
+
+unsigned int convert_o(va_list args, buffer_t *output,
+unsigned char flags, int wid, int prec, unsigned char len)
+{
+unsigned long int num;
+unsigned int ret = 0;
+char zero = '0';
+
+if (len == LONG)
+num = va_arg(args, unsigned long int);
+else
+num = va_arg(args, unsigned int);
+if (len == SHORT)
+num = (unsigned short)num;
+
+if (HASH_FLAG == 1 && num != 0)
+ret += _memcpy(output, &zero, 1);
+
+if (!(num == 0 && prec == 0))
+ret += convert_ubase(output, num, "01234567", flags, wid, prec);
+ret += print_neg_width(output, ret, flags, wid);
+
+return (ret);
+}
+
+/**
+ * convert_u - Converts an unsigned int argument to decimal and
+ *               stores it to a buffer contained in a struct.
+ * @args: A va_list pointing to the argument to be converted.
+ * @flags: Flag modifiers.
+ * @wid: A width modifier.
+ * @prec: A precision modifier.
+ * @len: A length modifier.
+ * @output: A buffer_t struct containing a character array.
+ * Return: The number of bytes stored to the buffer.
+ */
+
+unsigned int convert_u(va_list args, buffer_t *output,
+unsigned char flags, int wid, int prec, unsigned char len)
+{
+unsigned long int num;
+unsigned int ret = 0;
+
+if (len == LONG)
+num = va_arg(args, unsigned long int);
+else
+num = va_arg(args, unsigned int);
+if (len == SHORT)
+num = (unsigned short)num;
+
+if (!(num == 0 && prec == 0))
+ret += convert_ubase(output, num, "0123456789", flags, wid, prec);
+ret += print_neg_width(output, ret, flags, wid);
+
+return (ret);
+}
