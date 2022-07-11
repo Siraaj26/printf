67cd528c2cece29e45eb5b7ea9904ec9ffcc6d19
sashalaurel <enquiries@sashalaurel.net>
2022-07-11 16:59:23 -0700

    addition convert_p and comments update

--- convert_c_percent_p.c
+++ convert_c_percent_p.c
@@ -58,3 +58,32 @@ ret += _memcpy(output, &percent, 1);
 ret += print_neg_width(output, ret, flags, wid);
 return (ret);
 }
+
+/**
+ * convert_p - Converts address of an argument to hex and
+ * stores it to buffer contained in a struct.
+ * @args: va_list pointing to argument to be converted.
+ * @flags: Flag modifiers.
+ * @wid: Width modifier.
+ * @prec: Precision modifier.
+ * @len: Length modifier.
+ * @output: buffer_t struct containing character array.
+ * Return: Number of bytes stored to the buffer.
+ */
+
+unsigned int convert_p(va_list args, buffer_t *output,
+unsigned char flags, int wid, int prec, unsigned char len)
+{
+char *null = "(nil)";
+unsigned long int address;
+unsigned int ret = 0;
+(void)len;
+address = va_arg(args, unsigned long int);
+if (address == '\0')
+return (_memcpy(output, null, 5));
+flags |= 32;
+ret += convert_ubase(output, address, "0123456789abcdef",
+flags, wid, prec);
+ret += print_neg_width(output, ret, flags, wid);
+return (ret);
+}
